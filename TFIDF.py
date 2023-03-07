7fa0d1cc1835c5c410f2d6e0b3bbca61f3b8ddef
Ashlyn Dunn <97192252+ashlyn046@users.noreply.github.com>
2023-03-06 19:03:32 -0700

    Update TFIDF.py

--- TFIDF.py
+++ TFIDF.py
@@ -12,23 +12,8 @@ wordsDf = pd.read_csv('/content/gdrive/My Drive/RA/final.csv')
 # Term Frequency-Inverse Document Frequency (TF-IDF) Vector to convert to frequency vectors
 # show relative freq of diff terms by gender
 
-#max features makes it so it can't generate more than 500 features. ngram_range(1,1) says we only look at unigrams
+# ngram_range(1,1) only looking at unigrams initially
 tfidf = TfidfVectorizer(max_features=500, lowercase=True, analyzer='word', stop_words= 'english',ngram_range=(1,1))
 
 final_tfIdf = tfidf.fit_transform(wordsDf['processedtext'])
 print(final_tfIdf)
-
-df = pd.DataFrame(final_tfIdf.toarray(), columns = tfidf.get_feature_names())
-print(df)
-
-# tfidf = TfidfVectorizer(min_df=3)
-tfidf.fit(list(subject_sentences.values()))
-feature_names = tfidf.get_feature_names()
-
-
-# Now we can write the transformation logic like this
-def get_ifidf_for_words(text):
-    tfidf_matrix= tfidf.transform([text]).todense()
-    feature_index = tfidf_matrix[0,:].nonzero()[1]
-    tfidf_scores = zip([feature_names[i] for i in feature_index], [tfidf_matrix[0, x] for x in feature_index])
-    return dict(tfidf_scores)
